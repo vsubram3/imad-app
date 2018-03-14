c432dd8dd4e9d4d9b848c602e9a95ecab8aa5720
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-14 13:56:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Artice-one', function (req,res){
+app.get('/artice-one', function (req,res){
 res.send('Artice one requested  and will be served here');
 });
 
-app.get('/Article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article Two requested and will be served here');
 });
 
-app.get('/Article-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article Three requested and will be served here');
 });
 
