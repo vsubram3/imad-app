dca516e9f47e3b05d27e68179d60b98bb437a1dd
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-15 15:19:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,73 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title: 'Article One | Vimalkumar S',
+  heading: 'Article One',
+  date: '15-MAR-2018',
+  content: `<p>
+             This is my first article and learning new things,  But the errors are too much  Let me try my best
+            </p>
+          <p>
+            Hope things will get improved.
+          </p>
+        
+        <p>
+            Let me continue my journey of learning IMAD
+        </p>`
+
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var container = data.container;
+    var heading = data.heading;
+    var date = data.date;
+    
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />
+      
+    </head>
+    <body>
+        <div class = 'container'>
+        <div>
+            <a href = '\'> Home </a>
+        </div>
+        <hr/>
+        <h3>
+           ${heading}
+        </h3>
+         <div>
+            ${date} 
+         </div>
+        <div>
+        ${content}
+      </div>
+          </div>
+          </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-two', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-three', function (req, res) {
