c42af87f9d502b0fa7b626faaf20c30bcd6e0737
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-15 20:16:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-   res.send(createTemplate(articleOne));
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-three', function (req, res) {
