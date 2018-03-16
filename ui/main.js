4a6d938047492dd2b6df0ace25cd6e26b7d28bff
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-16 14:30:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innertext = "New Value";
+var element = document.getElementById("main-text").innerHTML;
+element.inneHTML = "New Value";
