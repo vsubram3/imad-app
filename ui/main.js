5ac1e0e53a6fedc991c3e46f68091106f0ee2e00
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-16 15:03:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerText = "New value";
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginleft='100px';
 };
