367028f5f8c921ab418bc8b158a39b6d3d58425c
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-19 22:46:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,18 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
-  counter = counter + 1; 
- var span = document.getElementById('count');
- span.innerHTML = counter.toString();
+    var request = newXMLHttpRequest();
+    
+  request.onreadystatechange = function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          if(request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count')
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+  request.open('GET','http://vimalnetworkengg.imad.hasura-app.io/', true);
+  request.send(null);
 };
