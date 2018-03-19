923119312cae12be21f42832d997a4c4c7fa95db
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-19 20:55:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var button = document.getElementbyId("counter");
 var counter=0;
 button.onclick = function(){
   counter = counter+1; 
- var span = documnet.getElementbyId("count");
+ var span = document.getElementbyId("count");
  span.innerHTML=counter.ToString();
 };
