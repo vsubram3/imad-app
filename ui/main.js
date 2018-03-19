8022acf6871429d311556e601fa55dd357d59524
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-19 22:51:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ button.onclick = function(){
           }
       }
   };
-  request.open('GET','http://vimalnetworkengg.imad.hasura-app.io/', true);
+  request.open('GET','http://vimalnetworkengg.imad.hasura-app.io/counter', true);
   request.send(null);
 };
