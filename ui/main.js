ad2c309def75b8a76ea179bea59688b9b02e537a
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-19 22:46:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function(){
       if(request.readyState===XMLHttpRequest.DONE){
           if(request.status === 200) {
               var counter = request.responseText;
-              var span = document.getElementById('count')
+              var span = document.getElementById('count');
               span.innerHTML = counter.toString();
           }
       }
