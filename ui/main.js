a2669d0fbe808e27dcb2117c6f44e836284d216c
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-20 08:02:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,13 +20,13 @@ button.onclick = function(){
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submi_btn');
-submit.onclick = function(){
+submit.onclick = function () {
     
     var names = ['name1','name2','name3','name4'];
     var list = '';
-    for (var i=0; i<names.length; i++) {
-        list += 'li' + names[i] + '<\li>';
+    for (var i=0; i< names.length; i++) {
+        list += '<li>' + names[i] + '<\li>';
     }
-    var url = document.getElementById('namelist');
+    var ul = document.getElementById('namelist');
     ul.inerHTML = 'list';
 };
