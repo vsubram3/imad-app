c1ac9cde1804fd86c1f1e3dc5ad3c262c4534210
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-20 11:55:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/counter', function(req,res){
     res.send(counter.toString());
 });
 
-var names = [];
+var names= [];
 app.get('/submit-name', function(req,res){
     var name = req.query.name;
     names.push(name);
