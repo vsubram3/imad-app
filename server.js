31f24746894389b3cd6cc5a6f5e1201e365fb410
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-20 11:59:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,14 +110,14 @@ var names = [];
 app.get('/submit-name', function(req,res){
     var name = req.query.name;
     names.push(name);
-    res.send(JSON.stringify[names]);
-    
-});
+    res.send(JSON.stringify(names));
+ });
 
  app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName; 
    res.send(createTemplate(articles[articleName]));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
