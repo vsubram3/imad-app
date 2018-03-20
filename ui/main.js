bea15bd91dfa26645e9a2b4ca3e326698469d94a
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-20 10:11:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,5 +27,5 @@ submit.onclick = function () {
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
-    ul.innerText = list;
+    ul.innerHTML=list;
 };
