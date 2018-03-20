c945bf1eb7469e3a768d203730620266e00784d0
vsubram3 <36990890+vsubram3@users.noreply.github.com>
2018-03-20 11:52:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/counter', function(req,res){
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
